f8d896f93732c46b64e152daafd550e37c7bd739
Russ <russ.thompson@outlook.com>
2020-11-13 16:20:04 -0800

    Rate limit fix (#3)
    * Update rate limit handling
    
    * Update docs
    
    * Increment version number
    
    * Simplify logic around logging

--- placekey/__init__.py
+++ placekey/__init__.py
@@ -1,2 +1,3 @@
 from .placekey import *
+__version__ = '0.0.7'
 __all__ = ['placekey', 'api']
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="placekey",
-    version="0.0.6",
+    version="0.0.7",
     author="SafeGraph Inc.",
     author_email="russ@safegraph.com",
     description="Utilities for working with Placekeys",
